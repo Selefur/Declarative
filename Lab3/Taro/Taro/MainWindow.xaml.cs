6019c0961f89fd0c245fc59e3ed077b76f6e73a8
Selefur <aleksandrabilous5@gmail.com>
2025-03-01 16:55:02 +0200

    First

--- Lab3/Taro/Taro/MainWindow.xaml.cs
+++ Lab3/Taro/Taro/MainWindow.xaml.cs
@@ -1,28 +1,48 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
-namespace Taro
+namespace FortuneTeller
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
+        private readonly List<string> cardImages = new List<string>
+        {
+            "Cards/Card0.jpg",
+            "Cards/Card1.jpg",
+            "Cards/Card2.jpg",
+            "Cards/Card3.jpg"
+        };
+        private readonly Random random = new Random();
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void PredictButton_Click(object sender, RoutedEventArgs e)
+        {
+            CardGrid.Children.Clear();
+            var selectedCards = new HashSet<string>();
+
+            while (selectedCards.Count < 3)
+            {
+                string card = cardImages[random.Next(cardImages.Count)];
+                selectedCards.Add(card);
+            }
+
+            foreach (var card in selectedCards)
+            {
+                Image img = new Image
+                {
+                    Source = new BitmapImage(new Uri(card, UriKind.Relative)),
+                    Height = 150,
+                    Stretch = System.Windows.Media.Stretch.Uniform
+                };
+                CardGrid.Children.Add(img);
+            }
         }
     }
 }
