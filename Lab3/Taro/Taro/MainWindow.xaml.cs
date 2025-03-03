edaa7f3ca7b1663b237986f9ca13d14ccf326170
Selefur <aleksandrabilous5@gmail.com>
2025-03-03 17:33:19 +0200

    Functional

--- Lab3/Taro/Taro/MainWindow.xaml.cs
+++ Lab3/Taro/Taro/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Media.Imaging;
@@ -13,36 +14,60 @@ namespace FortuneTeller
             "Cards/Card0.jpg",
             "Cards/Card1.jpg",
             "Cards/Card2.jpg",
-            "Cards/Card3.jpg"
+            "Cards/Card3.jpg",
+            "Cards/Card4.jpg",
+            "Cards/Card5.jpg",
+            "Cards/Card6.jpg",
+            "Cards/Card7.jpg",
+            "Cards/Card8.jpg"
         };
+
         private readonly Random random = new Random();
+        private readonly List<string> defaultCards = new List<string> { "Cards/Card01.jpg", "Cards/Card02.jpg", "Cards/Card03.jpg" };
 
         public MainWindow()
         {
             InitializeComponent();
+            LoadDefaultCards();
+        }
+
+        private void LoadDefaultCards()
+        {
+            CardGrid.Children.Clear();
+            foreach (var card in defaultCards)
+            {
+                CardGrid.Children.Add(CreateImage(card));
+            }
         }
 
         private void PredictButton_Click(object sender, RoutedEventArgs e)
         {
             CardGrid.Children.Clear();
+
+            // Виключаємо стартові картинки з можливих варіантів
+            var availableCards = cardImages.Except(defaultCards).ToList();
             var selectedCards = new HashSet<string>();
 
-            while (selectedCards.Count < 3)
+            while (selectedCards.Count < 3 && availableCards.Count > 0)
             {
-                string card = cardImages[random.Next(cardImages.Count)];
+                string card = availableCards[random.Next(availableCards.Count)];
                 selectedCards.Add(card);
             }
 
             foreach (var card in selectedCards)
             {
-                Image img = new Image
-                {
-                    Source = new BitmapImage(new Uri(card, UriKind.Relative)),
-                    Height = 150,
-                    Stretch = System.Windows.Media.Stretch.Uniform
-                };
-                CardGrid.Children.Add(img);
+                CardGrid.Children.Add(CreateImage(card));
             }
+        }
+
+        private Image CreateImage(string path)
+        {
+            return new Image
+            {
+                Source = new BitmapImage(new Uri(path, UriKind.Relative)),
+                Height = 600,
+                Stretch = System.Windows.Media.Stretch.Uniform
+            };
         }
     }
 }
